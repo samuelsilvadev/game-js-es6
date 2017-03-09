0885650212c1073cceceffba9536fa1c403f2535
samuel <samuelsilvawb@gmail.com>
2017-03-09 20:36:14 -0300

    adicionando imagem do heroi

--- gameEs6.js
+++ gameEs6.js
@@ -9,10 +9,18 @@ canvas.height = 512;
 
 document.body.appendChild(canvas);
 
-//carregando imagem
+//carregando imagem de fundo
 let bgReady = false;
 const bgImage = new Image();
 bgImage.addEventListener('load', () => {
 	bgReady = true;
 });
 bgImage.src = 'imgs/background.jpg';
+
+//carregando imagem do heroi
+let heroReady = false;
+const heroImage = new Image();
+heroImage.addEventListener('load', () => {
+	heroReady = true;
+});
+heroImage.src = 'imgs/hero.jpg';
