0ef7bd8411055c0a7d8e67cdff34a6aeaa90168b
samuel <samuelsilvawb@gmail.com>
2017-03-09 20:27:58 -0300

    adicionando imagem de fundo

--- gameEs6.js
+++ gameEs6.js
@@ -1,6 +1,18 @@
 
+
+//criando canvas 
 const canvas = document.createElement('canvas');
 const ctx = canvas.getContext('2d');
 
 canvas.width = 512;
 canvas.height = 512;
+
+document.body.appendChild(canvas);
+
+//carregando imagem
+let bgReady = false;
+const bgImage = new Image();
+bgImage.addEventListener('load', () => {
+	bgReady = true;
+});
+bgImage.src = 'imgs/background.jpg';
