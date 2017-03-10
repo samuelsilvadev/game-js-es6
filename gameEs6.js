296dd195bdca6e97fb48669ae932c0d87fd70bac
samuel <samuelsilvawb@gmail.com>
2017-03-10 20:12:24 -0300

    adicionando função que valida teclas (setas) pressionadas e função que verrifica se os monstros bateram

--- gameEs6.js
+++ gameEs6.js
@@ -60,3 +60,32 @@ const reset = function(){
 	monster.x = 32 + (Math.random() * (canvas.width - 64))
 	monster.y = 32 + (Math.random() * (canvas.height - 64))
 }
+
+//função que ira atualizar os objetos do jogo
+const updateObjects = function(modifier){
+	if(38 in keysDown){//seta para cima
+		hero.y -= hero.speed * modifier;
+	}
+	if(38 in keysDown){//seta para baixo
+		hero.y += hero.speed * modifier;
+	}
+	if(37 in keysDown){//seta para esquerda
+		hero.x -= hero.speed * modifier;
+	}
+	if(39 in keysDown){//seta para direita
+		hero.x += hero.speed * modifier;
+	}
+	verifyObjectsSamePosition();
+}
+
+const verifyObjectsSamePosition = function(){
+	if(
+		hero.x <= (monster.x + 32)
+		&& monster.x <= (hero.x + 32)
+		&& hero.y <= (monster.y + 32)
+		&& monster.y <= (hero.y + 32)
+	){
+		++monstersCaught;
+		reset();
+	}
+}
